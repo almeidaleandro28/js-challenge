08a3096699d5e98c36e11ac8e4f1ebbedb3b73df
leandro almeida <almeidaleandro.silva@gmail.com>
2022-06-03 17:40:51 -0300

    add new problem

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+// problem 1 - Simple Sum
+// https://www.beecrowd.com.br/judge/en/problems/view/1003
+const A = 30;
+const B = 10;
+const SOMA = A + B;
 
+console.log( `SOMA  = ${SOMA}` );
+
+//problem 2 - Simple product
+//https://www.beecrowd.com.br/judge/en/problems/view/1004
+// 3 * 9 = 27
+const number1 = prompt( "digit number one");
+const number2 = prompt( " digit number two" );
+const PROD = number1 * number2;
+
+console.log( "PROD ", PROD );
