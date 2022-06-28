841c8f2ae2db1632b3d6f10a187aed271adbf1c7
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2022-06-28 19:17:33 -0300

    first challenge

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+//2 3 = false
+const testNumber = ( a, b ) => ( a === b ) ? true : false;
+
+console.log( testNumber( "3", "3" ) );
