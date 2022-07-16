c625582acb6e51e5c32ff91c9443c757c925613c
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2022-07-16 19:44:11 -0300

    finish challenge 08

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// ======================== Basics ===========================================================
 // ====================== https://www.jschallenger.com/ ========================================
 // Write a function that takes two values, say a and b, as arguments
 // Return true if the two values are equal and of the same type
@@ -20,3 +21,38 @@ console.log( challange02( 99, 1 ) );
 //2,3 = false -  3,3 = true -  1,"1" = false
 const challange03 = ( a, b ) => a === b ? `${a} equal and same type  ${b}`: `${a} not equal and not same equal ${b}`;
 console.log( challange03( 1, 1)); 
+
+//=================================== *** ==========================================================================
+// Write a function that takes a value as argument
+// Return the type of the value
+// 1 = number - false - boolean
+const challange04 = ( a ) =>  typeof a;
+console.log( challange04( false ) );
+
+//================================= *** ==================================================================================
+// Write a function that takes a string (a) and a number (n) as argument
+// Return the nth character of 'a'
+//"adcd",1 = a - ('zyxbwpl',5) = w
+const challange05 = ( word, num ) => word[ num ];
+
+console.log( "challenge 05 - result = ", challange05( "leandro", 0) );
+//==================================***==========================================================================================
+//Write a function that takes a string (a) as argument
+// Get the first 3 characters of a
+// Return the result a = abcdefg = abc
+const challange06 = ( word ) => word.slice( 0, 3 );
+console.log( "challenge 06 - result =", challange06( "abcdefg" ) );
+// ====================================***===================================================================================
+// Write a function that takes a string as argument
+// Extract the last 3 characters from the string
+// Return the result
+const challange07 = ( word ) => word.slice( -3 );
+console.log( "challenge 07 - result = ", challange07( "abcdefg" ) );
+// ====================================***==================================================================================
+// Write a function that takes a string (a) as argument
+// Extract the first half a
+// Return the result abcdefgh = abcd
+const challange08 = ( word ) => word.slice( 0, word.length / 2);
+console.log( "challenge 08 - result = ", challange08( "abcdefgh" ) );
+
+
