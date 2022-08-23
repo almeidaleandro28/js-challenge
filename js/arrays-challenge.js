13487982d235f5cd49571780a84eb895add6b2a1
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2022-08-23 19:44:46 -0300

    update challege array 5

--- js/arrays-challenge.js
+++ js/arrays-challenge.js
@@ -1,3 +1,5 @@
+const show = ( msg ) => console.log( msg );
+
 // =============== array challenges =================================
 // Write a function that takes an array of strings as argument
 // Sort the array elements alphabetically
@@ -30,3 +32,20 @@ console.log( "challenge 03 ", resulChallenge3);
 const arrayChallenge4 = ( arr, n ) => arr.filter( numb => numb !== n );
 const resulChallenge4 = arrayChallenge4( [1,2,3], 2);
 console.log("challenge 04 ", resulChallenge4);
+// ==============================***===============================================
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+//[1,2,2,4] - 4
+const arrayChallenge5 = ( arr ) => arr.length;
+const resulChallenge5 = arrayChallenge5( [1,2,2,4]);
+console.log( "challenge 05", resulChallenge5);
+// ================================= *** ========================================
+// Write a function that takes an array (a) and a value (n) as arguments
+// Save every nth element in a new array
+// Return the new array
+//[1,2,3,4,5,6,7,8,9,10],3 => [3,6,9]
+const arrayChallenge6 = ( arr, n ) =>  { 
+  arr[n]
+};
+const resulChallenge6 = arrayChallenge6( [1,2,3,4,5,6,7,8,9,10], 3 );
+show( "challenge 06 ", resulChallenge6 );
